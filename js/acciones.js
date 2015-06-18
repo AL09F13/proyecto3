4b4df232538965e5205c6531789c9c7ab0df3824
AL09F13 <c4rt3r97@gmail.com>
2015-06-18 04:16:14 -0500

    uno
    proyecto tercer parcial

---
+++ js/acciones.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	
+	
+	$('#tomorrow').on ('tap', function() {
+	navigator.vibrate(400);
+	
+	$('#worldd').on ('tap', function() {
+	navigator.vibrate(400);
+	
+	$('#ultra').on ('tap', function() {
+	navigator.vibrate(400);
+	
+	$('#parties').on ('tap', function() {
+	navigator.vibrate(400);
+	
+	$('#electric').on ('tap', function() {
+	navigator.vibrate(400);
+	
+	
+});  // tap btn_vibrar
+});  // tap btn_vibrar
+});  // tap btn_vibrar
+});  // tap btn_vibrar
+});  // tap btn_vibrar
+	
+	
+}); 
+});
+
