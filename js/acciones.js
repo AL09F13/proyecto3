baca8376f3185e35bbfee8119dc70d1025fc79a5
AL09F13 <c4rt3r97@gmail.com>
2015-06-24 14:56:30 -0500

    tres
    acciones

--- js/acciones.js
+++ js/acciones.js
@@ -5,25 +5,60 @@ document.addEventListener("deviceready",function(){
 	
 	$('#tomorrow').on ('tap', function() {
 	navigator.vibrate(400);
+
+});  // tap btn_vibrar
 	
 	$('#worldd').on ('tap', function() {
 	navigator.vibrate(400);
+
+});  // tap btn_vibrar
 	
 	$('#ultra').on ('tap', function() {
 	navigator.vibrate(400);
+
+});  // tap btn_vibrar
 	
 	$('#parties').on ('tap', function() {
 	navigator.vibrate(400);
+
+});  // tap btn_vibrar
 	
 	$('#electric').on ('tap', function() {
 	navigator.vibrate(400);
 	
-	
 });  // tap btn_vibrar
-});  // tap btn_vibrar
-});  // tap btn_vibrar
-});  // tap btn_vibrar
-});  // tap btn_vibrar
+
+
+   $('#inicio').on ('tap' , function() {
+	  navigator.notification.beep(1);
+	  
+
+}); //tap btnbeep
+
+ $('#inicio2').on ('tap' , function() {
+	  navigator.notification.beep(1);
+	  
+
+}); //tap btnbeep
+
+ $('#inicio3').on ('tap' , function() {
+	  navigator.notification.beep(1);
+	  
+
+}); //tap btnbeep
+
+ $('#inicio4').on ('tap' , function() {
+	  navigator.notification.beep(1);
+	  
+
+}); //tap btnbeep
+
+ $('#inicio5').on ('tap' , function() {
+	  navigator.notification.beep(1);
+	  
+
+}); //tap btnbeep
+
 	
 	
 }); 
